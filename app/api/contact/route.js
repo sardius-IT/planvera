81e2e583c3644707e1561ff016b6c979f4b6af04
Sardius-IT <info@sardius.co.in>
2025-08-22 12:12:58 +0530

    upde

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -7,8 +7,8 @@ export async function POST(request) {
     const transporter = nodemailer.createTransport({
       service: "Gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+         user: "planveraconsultancy@gmail.com",
+        pass: "ytqjvpynvedndtdr",
       },
       tls: {
         rejectUnauthorized: false,
